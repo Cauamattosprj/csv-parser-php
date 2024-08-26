47d64035e661972d8f35780505c2d9807775d5e0
Caua Mattos - Debian <caua.h4k@gmail.com>
2024-08-26 20:47:34 -0300

    comentarios adicionados

--- app/App.php
+++ app/App.php
@@ -2,15 +2,19 @@
 
 declare(strict_types = 1);
 
+# Função para adicionar o conteúdo CSV em table datas HTML. Essa função é chamada dentro de 'views/transactions.php'.
 function table_data() {
+    # trecho demonstrativo da função fgetcsv() retirado diretamente da documentação oficial do PHP, adaptado com a lógica necessária para essa aplicação.
     if (($handle = fopen(TRANSACTIONS_PATH . 'sample_1.csv', "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
             echo "<tr>";
             for ($c=0; $c < 4; $c++) {
                 switch (true) {
+                    # Quando o iterador $c passar pelo índice 3 do array $data (amount) a variável $transaction_value passa por dois str_replace para correta formatação para os cálculos.
                     case $data[$c] == $data[3]:
                         $transaction_value = str_replace('$', '', $data[3]);
                         $transaction_value = str_replace(',', '', $transaction_value);
+                        # se o valor for negativo, a cor passa a ser vermelha.
                         if (floatval($transaction_value) < 0) {
                             echo "<td style='color: red'>$data[$c]</td>";
                         } else {
@@ -31,7 +35,7 @@ function table_data() {
     }      
 
 }
-
+# calculos para o total líquido das transações
 function table_net_total() {
     if (($handle = fopen(TRANSACTIONS_PATH . 'sample_1.csv', "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
@@ -44,6 +48,7 @@ function table_net_total() {
     }
 }
 
+# total de receitas
 function table_total_income() {
     if (($handle = fopen(TRANSACTIONS_PATH . 'sample_1.csv', 'r')) !== FALSE) {
         while (($data = fgetcsv($handle, 0,',')) !== FALSE) {
@@ -58,6 +63,7 @@ function table_total_income() {
     }
 }
 
+# total de despesas
 function table_total_expense() {
     if (($handle = fopen(TRANSACTIONS_PATH . 'sample_1.csv', 'r')) !== FALSE) {
         while (($data = fgetcsv($handle, 0,',')) !== FALSE) {
